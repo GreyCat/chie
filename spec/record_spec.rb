f6d1d8368f915c5fab13452f91829fe19b27d503
Mikhail Yakshin <greycat@altlinux.org>
2015-11-06 17:00:37 +0300

    More specs for Record

--- spec/record_spec.rb
+++ spec/record_spec.rb
@@ -1,6 +1,36 @@
 require 'spec_helper'
 
 describe Record do
+  SIMPLE_PERSON_SCHEME = {
+    'attr' => [
+      {'name' => 'name', 'type' => 'str', 'ind' => true},
+    ]
+  }
+
+  SIMPLE_BOOK_SCHEME = {
+    'attr' => [
+      {'name' => 'name', 'type' => 'str', 'len' => 100, 'ind' => true},
+      {'name' => 'yr', 'type' => 'int', 'ind' => true},
+      {'name' => 'comm', 'type' => 'str'},
+    ],
+    'rel' => [
+      {'name' => 'author', 'target' => 'person', 'type' => '1'},
+    ],
+  }
+
+  before(:all) do
+    sqlclear
+    @e = Engine.new(CREDENTIALS)
+    @e.entity_create(Entity.new('person', SIMPLE_PERSON_SCHEME))
+    @e.entity_create(Entity.new('book', SIMPLE_BOOK_SCHEME))
+
+    @person_id = @e.entity!('person').insert({'name' => 'John Smith'})
+    @book_id = @e.entity!('book').insert({'name' => 'Foo book', 'yr' => 4321, 'comm' => 'Blah', 'author' => @person_id})
+
+    @person_rec = @e.entity!('person').get(@person_id)
+    @book_rec = @e.entity!('book').get(@book_id)
+  end
+
   describe '#==' do
     it 'returns true for two different objects with equal contents' do
       r1 = Record.new({'a' => 1, 'b' => 2})
@@ -15,5 +45,18 @@ describe Record do
 
       expect(r1 == r2).to eq(false)
     end
+  end
+
+  describe '#[attr]' do
+    it 'gets scalar value in indexed column' do
+      expect(@book_rec['name']).to eq('Foo book')
+    end
+
+    it 'gets scalar value in non-indexed column' do
+      expect(@book_rec['comm']).to eq('Blah')
+    end
+  end
+
+  describe '#[rel]' do
   end
 end
